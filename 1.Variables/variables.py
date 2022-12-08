e45c2dceeb8321ea7e709a05f9336ce4148c35fb
Arturo Lorenzo <35840182+smitexx@users.noreply.github.com>
2022-12-08 23:53:45 +0100

    finish variables notes

--- 1.Variables/variables.py
+++ 1.Variables/variables.py
@@ -29,13 +29,28 @@ igual_que = 1 == 1
 mayor_o_igual_que = 2 >= 1
 menor_o_igual_que = 1 <= 2
 distinto_que = 2 != 1
+#OPERADORES LOGICOS (Devuelven un tipo de dato booleano)
+and_operation = True and False #False
+or_operation = True or False #True
+not_operation = not False #True
+
+#OPERADOR DE PERTENENCIA (Devuelve un valor booleano) Estos operadores aplican a las secuencias. (Strings, listas y tuplas)
+in_operation = "hola" in "hola mundo" #True
+not_in_operation = "hola" not in "hola mundo" #False
+
+#OPERADOR DE IDENTIDAD (Devuelve valor booleano) Sirve para comprobar si dos variables emplean la misma ubicación en memoria.
+x = 1
+y = x
+is_operation = x is y #True
+is_not_operation = x is not y #False
 
 #(CURIOSIDADES) OPERADORES BIT A BIT (Se usa 2 = 10 y 3 = 11 en binario para hacer las operaciones de ejemplo. )
 and_bit_a_bit = 2 & 3 # 10 & 11 = 10 (2)  se hace and bit a bit el (1&1 = 1) y (0&1= 0)
 or_bit_a_bit = 2 | 3 # 10 | 11 = 11 (3)
 xor_bit_a_bit = 2 ^ 3 # 10 ^ 11 = 01 (1)
-not_bit_a_bit  = ~2 # ~1~0 = 01 (1)
-
+not_bit_a_bit  = ~2 # ~10 = Complemento a 2 negación.
+right_shift_bit_a_bit = 2 >> 3 #Se desplazan 3 bits a la derecha el 10 = 000 (0)
+left_shift_bit_a_bit = 2 << 3 #0000010 << 00000011 = 0010000 (16) se le meten 4 bits a la derecha del 10
 
 #Estructuras de datos básicas
 mi_lista : list = ['Hola', 'Mundo']
