ad930d6dc7f997f87c348dbd3fb293ebf1e0b2e4
Arturo Lorenzo <35840182+smitexx@users.noreply.github.com>
2022-12-10 15:43:32 +0100

    finishing lists and sets

--- 2.Data-structures/lists.py
+++ 2.Data-structures/lists.py
@@ -1,53 +1,82 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
+#Las listas hacen uso de arrays de longitud variable internamente, los arrays de longitud variable son una variante de los arrays 
+#cuyo tamaño no se especifica inicialmente, o cuya longitud o tamaño se establece durante el tiempo de ejecución, 
+#también puede llamarlos arreglos automáticos, se van redimensionando según las necesidades del programa, se dicen arrays automáticos porque cada vez que se hace un append 
+#se guardan en memoria más espacio del que se necesita para prevenir redimensionar muchas veces.
 
-################### INSERT #####################
+#Enlace de la web: https://towardsdatascience.com/how-lists-in-python-are-optimised-internally-for-better-performance-847c8123b7fa
 
-# Append in the last position of the list
+#Las listas en Python son ordenadas, quiere decir que cada vez que se añade un elemento tiene un orden definido, se va al final de la lista. Y cada vez que se borra un elemento
+#se corren los elementos de la derecha una posición a la izquierda. [0,1,2,3,4] -> Quitamos el 2 -> [0,1,3,4]
+#Las lista son cambiantes, es decir, se pueden actualizar los valores de sus elementos. 
+#Las listas permiten valores duplicados.
+
+################### 1.CREACIÓN DE LISTAS #####################
+# 1. Lista vacía
+lista_vacia = []
+lista_vacia = list()
+# 2. Lista con elementos
+motorcycles = ['honda', 'yamaha', 'suzuki','honda']
+
+################### 2.INSERCIONES Y ACTUALIZACIONES #####################
+# 1. Añade un elemento al final de la lista
 motorcycles.append('ducati')
-# Insert element in specific position of the list
+# 2. Añade un elemento en una posición de la lista, empezando por 0.
 motorcycles.insert(0, 'daelim')
+# 3. Actualizar una lista es muy fácil, sólo tienes que sobreescribir la posición. 
+motorcycles[1] = 'bmw'
 
-################### DELETIONS #####################
-
-# Remove specific element of the list
-del motorcycles[1]
-# Also we can remove specific element with pop and get the value deleted.
+################### 3.BORRADOS #####################
+# 1. Tambien podemos borrar un elemento especifico con pop y obtener el valor borrado.
 element = motorcycles.pop(0)
-# Remove the last element of the list and it return it.
+# 2. Por defecto borrar el ultimo elemento de la lista.
 last_element = motorcycles.pop()
-# Remove element by value (The remove() method deletes only the first occurrence of the value you specify)
+# 3. Borrar un elemento de la lista, solo borra la primera apariencia del elemento indicado y no devuelve nada.
 motorcycles.remove('yamaha')
-# Removing All Instances of Specific Values from a List
-pets = ['dog', 'cat', 'dog', 'bull', 'cat', 'rabbit', 'cat']
-print(pets)
-while 'cat' in pets:
-    pets.remove('cat')
+# 4. Borrar un elemento de la lista.
+while 'honda' in motorcycles:
+    motorcycles.remove('honda')
+# 5. Borra todos los elementos de una lista
+motorcycles.clear()
 
-print(pets)
+################# 4.SORT ######################
+# 1. Ordenar alfabeticamente
+vocales = ['e','i','o','u','a']
+vocales.sort()
+vocales.sort(reverse=True)
+# 2. Si queremos ver un lista ordenada pero no ordenarla realmente. 
+sorted(vocales)
+# 3. Darle la vuelta a una lista
+vocales.reverse()
+# 4. Le podemos asignar un criterio de ordenación.
+def order_by_length(element):
+  return len(element)
+cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+cars.sort(key=order_by_length)
 
-################# SORT ######################
+################# 5.OTROS MÉTODOS ######################
+my_foods = ['pizza', 'tacos', 'kebab', 'pizza', 'hamburguesa', 'pizza', 'tacos']
+# 1. IMPORTANT [:] Importante no asignar una lista a otra variable para copia la lista, 
+# van a apuntar a la misma zona de memoria (friends_foods = my_foods no funciona)
+copy_of_food = my_foods[:]
+copy_of_food_version_two = my_foods.copy()
+# 2. Contar el número de elementos de un valor específico.
+pizza_number = my_foods.count('pizza')
+# 3. Añadir una lista a otra lista. 
+my_foods.extend(['ensalada', 'manzana'])
+my_foods = my_foods + ['ensalada', 'manzana']
+# 4. Devuelve el indice de la primera aparición de un elemento de la lista.
+my_foods.index('pizza') #Devuelve 0
 
-# Sort alphabetical or reverse permantly
-motorcycles.sort()
-motorcycles.sort(reverse=True)
-# Sort list temporaly
-sorted(motorcycles)
-# Give reverse to the list (Darle la vuelta a una lista)
-motorcycles.reverse()
 
-
-# MAKING NUMERICAL LISTS range(x,y-1) El tipico for (i; i < x; i++) eso en python no existe.
-for value in range(1, 5):
+# 5. Rangos númericos con la función range(x,y-1) El tipico for (i; i < x; i++) eso en python no existe.
+for value in list(range(1, 5)):
     print(value)
 
 even_numbers = list(range(2, 11, 2))  # Lista de pares del 2 al 10
 
-min(even_numbers)  # Minimum
-max(even_numbers)  # Maximum
-sum(even_numbers)  # Sum
+# 6. Funciones Built-in de python que se pueden aplicar sobre las listas
+min(even_numbers)  # Minimo de una lista
+max(even_numbers)  # Maximo de una lista
+sum(even_numbers)  # Suma los elementos de la lista
+len(even_numbers)  # Longitud de la lista
 
-################# COPY LIST ######################
-
-my_foods = ['pizza', 'falafel', 'carrot cake']
-# IMPORTANT [:] if not they will point to the same list (friends_foods = my_foods doesn't work)
-friend_foods = my_foods[:]
---
+++ 2.Data-structures/set.py
@@ -0,0 +1,54 @@
+#La estructura de datos utilizada en SET es Hashing, una técnica popular para realizar inserción, eliminación y recorrido en O(1) en promedio. 
+#Las operaciones en Hash Table son algo similares a las de Linked List. Los sets en python son una lista desordenada con elementos duplicados eliminados.
+#Un set es un mapa hash con linked list desordenadas. Por eso es mejor utilizar set que list cuando el orden de inserción y eliminación nos den igual.
+
+#Enlace en la web: https://www.codesansar.com/python-programming/how-sets-are-internally-stored-python.htm
+
+#Los sets establecidos no están ordenados, es decir, cuando se añade un elemento o se borra no se sabe en que posición del set va a caer.
+#Los sets no se pueden modificar. 
+#Los sets no permiten valores duplicados.
+
+
+################### 1.CREACIÓN DE SETS #####################
+# 1. set vacío
+nuevo_set = set()
+# 2. Inicializar un set con elementos
+nuevo_set = {'uno','dos'}
+
+################### 2.INSERTAR Y ACTUALIZAR #####################
+nuevo_set.add('uno') #Añade un elemento, solo puedes pasar un elemento, no un set.
+nuevo_set.update({'dos','tres'}) #Actualiza un set con otro set, añade los elementos que no están en el primer set.
+copia_set = nuevo_set.copy() #Crea una copia del set.
+
+################### 3.BORRADOS #####################
+nuevo_set.discard('dos') #Borra el elemento, pero NO lanza un error si no existe
+nuevo_set.remove('tres') #Borra el elemento, pero lanza un error si no existe
+nuevo_set.clear() #Borra todos los elementos del set
+
+################### 4.OPERACIONES DE SETS #####################
+set_uno = {1,2,3,4}
+set_dos = {4,5,6,7}
+# ***Los siguientes métodos devuelven un conjunto nuevo, no modifican el conjunto. ***
+
+set_diferencia = set_uno.difference(set_dos) #Devuelve un conjunto con los elementos que están en el set_uno y no en el set_dos, en este caso {1,2,3}
+set_diferencia_simetrica = set_uno.symmetric_difference(set_dos) #Devuelve todos los elementos que están en el primer conjunto y en el segundo conjunto, excepto los que son comunes. 
+                                                                 #En este caso {1,2,3,5,6,7}, el 4 es común, es como una antiintersección. 
+                                                                 
+set_interseccion = set_uno.intersection(set_dos) #Devuelve un conjunto con los elementos que están en ambos conjuntos, en este caso {4}
+set_union = set_uno.union(set_dos) #Devuelve un conjunto con la del conjunto uno con el conjunto dos, en este caso {1,2,3,4,5,6,7}
+
+# ***Los siguientes métodos modifican el conjunto sobre el que se aplica el método. ***
+
+set_uno.difference_update(set_dos) #La variable set_uno contiene {1,2,3} ahora, es decir, la diferencia entre set_uno y set_dos. 
+set_uno = {1,2,3,4} #Reestablecemos el conjunto con los valores iniciales.
+set_uno.intersection_update(set_dos) #La variable set_uno contiene {4} ahora, es decir, la intersección entre set_uno y set_dos.
+set_uno = {1,2,3,4} #Reestablecemos el conjunto con los valores iniciales.
+set_uno.symmetric_difference_update(set_dos) #La variable set_uno contiene {1,2,3,5,6,7}, es decir, la diferencia simetrica entre set_uno y set_dos.
+print(set_uno)
+
+################### 5.OTROS METODOS #####################
+no_tiene_interseccion = set_uno.isdisjoint(set_dos) #Devuelve true si NO tienen elementos compartidos, es decir, si la intersección es vacía. En este caso devuelve FALSE.
+es_subconjunto = set_uno.issubset(set_dos) #Devuelve true si el primer conjunto es subconjunto del set dos. En este caso es FALSE.
+es_superconjunto = set_uno.issuperset(set_dos) #Devuelve true si el segundo conjunto es subconjunto del set uno. En este caso es FALSE.
+
+
