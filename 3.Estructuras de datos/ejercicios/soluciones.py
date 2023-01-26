438d8b67c4baac2d53fe30a92626b8d68754e5b9
Arturo Lorenzo <arturo.lorenzo.h@hotmail.com>
2023-01-26 18:05:19 +0100

    soluciones estructuras de datos y funciones

--- 3.Estructuras de datos/ejercicios/soluciones.py
+++ 3.Estructuras de datos/ejercicios/soluciones.py
@@ -11,18 +11,59 @@
 # 7.Imprime la longitud de la lista usando "len()".
 # 8.Usando un bucle "for", imprime cada producto de la lista en orden inverso.
 
+compras = ['leche','pan','huevos','manzanas','queso']
+
+for compra in compras:
+    print(f'Producto: {compra}')
+
+compras.append('cereales')
+compras.insert(0,'sal')
+compras.remove('queso')
+ultimo_elemento = compras.pop()
+
+print(f'La longitud de la lista es {len(compras)}.')
+
+for compra in compras[::-1]:
+    print(f"Producto en orden inverso: {compra}")
+
+
 ################### EJERCICIO 2 #####################
 
-# 1.Crea una lista llamada "empleados" que contenga 3 listas vacías. Cada lista vacía representa un departamento (por ejemplo, ventas, marketing, tecnología).
-# 2.Usando un bucle "for", añade 3 nombres a cada una de las listas vacías dentro de "empleados". Cada nombre debe ser un string.
-# 3.Crea una variable "total_empleados" e inicialízala en cero. Usando un bucle anidado (dentro de otro bucle), recorre cada nombre de cada lista dentro de "empleados" y 
+# 1.Crea una lista llamada "empleados" que contenga 3 listas con 3 nombres de persona cada una. Cada lista vacía un departamento (por ejemplo, ventas, marketing, tecnología).
+# 2.Crea una variable "total_empleados" e inicialízala en cero. Usando un bucle anidado (dentro de otro bucle), recorre cada nombre de cada lista dentro de "empleados" y 
 # suma 1 a la variable "total_empleados".
-# 4.Usando la función "append()", agrega un nuevo empleado en una lista específica (por ejemplo, en el departamento de tecnología).
-# 5.Usando la función "remove()", elimina un empleado específico de una lista dentro de "empleados" (por ejemplo, el empleado "Juan" en el departamento de ventas).
-# 6.Usando la función "pop()", elimina el último empleado de una lista dentro de "empleados" (por ejemplo, del departamento de marketing) y guarda su valor en una variable llamada "ultimo_empleado".
-# 7.Imprime la longitud de "empleados" usando "len()" y la longitud de cada una de las listas dentro de "empleados" usando un bucle anidado.
-# 8.Usando un bucle anidado, imprime el nombre de cada empleado en cada departamento.
+# 3.Usando la función "append()", agrega un nuevo empleado en una lista específica (por ejemplo, en el departamento de tecnología).
+# 4.Usando la función "remove()", elimina un empleado específico de una lista dentro de "empleados" (por ejemplo, el empleado "Juan" en el departamento de ventas).
+# 5.Usando la función "pop()", elimina el último empleado de una lista dentro de "empleados" (por ejemplo, del departamento de marketing) y guarda su valor en una variable llamada "ultimo_empleado".
+# 6.Imprime la longitud de "empleados" usando "len()" y la longitud de cada una de las listas dentro de "empleados" usando un bucle anidado sin utilizar "len()", debes utilizar una variable
+#   auxiliar llamada "numero_empleados_departamento" inicializada a 0.
+# 7.Usando un bucle anidado, imprime el nombre de cada empleado en cada departamento.
 
+empleados = [['Juan','Arturo','Maria'],['Alberto','Pedro','Ana'],['Andrea','Cristina','Pepe']] #Ventas, marketing, tecnologia
+total_empleados = 0
+
+for departamento in empleados:
+    for empleado in departamento:
+        total_empleados = total_empleados + 1
+        
+print(total_empleados)
+
+empleados[2].append('Amador')
+empleados[0].remove('Juan')
+ultimo_empleado_marketing = empleados[1].pop()
+
+print(f"El numero total de departamentos son: {len(empleados)}")
+
+numero_empleados_departamento = 0
+for departamento in empleados:
+    numero_empleados_departamento =0
+    for empleado in departamento:
+        numero_empleados_departamento = numero_empleados_departamento + 1
+    print(f"El numero de empleados del departamento {empleados.index(departamento)} son {numero_empleados_departamento}")
+    
+        
+
+    
 ################### EJERCICIO DE SETS #####################
 
 # 1.Crea un set llamado "amigos_invitados" con los nombres de tus amigos a los que has invitado a una fiesta (por ejemplo: Juan, Pedro, Ana, Marta, Carlos).
@@ -34,15 +75,45 @@
 # 7.Usando la función "clear()" vacía el set "amigos_invitados".
 # 8.Usando la función "len()" imprime la cantidad de elementos en "amigos_confirmados".
 
+amigos_invitados = {'Juan', 'Pedro', 'Ana', 'Marta', 'Carlos'}
+amigos_confirmados = {'Juan', 'Pedro', 'Ana'}
+
+amigos_confirmados.add('Luis')
+
+amigos_sin_confirmar = amigos_invitados.difference(amigos_confirmados)
+amigos_confirmados = amigos_invitados.intersection(amigos_confirmados)
+amigos_invitados.discard('Marta')
+amigos_invitados.clear()
+
+print(len(amigos_confirmados))
+
+
 ################### EJERCICIO DE TUPLAS #####################
 
 # 1.Crea una tupla llamada "direccion" con tus datos de dirección (por ejemplo: calle, número, ciudad, código postal, país).
 # 2.Usando la función "index()" imprime el índice de la ciudad en la tupla "direccion".
 # 3.Usando acceso por intervalos, imprime solo el código postal y el país de tu dirección.
-# 4.Crea una variable "nueva_direccion" igual a "direccion" y cambia el número de tu dirección en la variable "nueva_direccion".
+# 4.Crea una variable "nueva_direccion" igual a "direccion" y cambia el número de tu dirección en la variable "nueva_direccion", 
+#   es necesario cambiar la tupla a lista para modificarla.
 # 5.Usando la función "count()" cuenta cuantas veces aparece la ciudad en la tupla "direccion".
 # 6.Usando la función "len()" imprime la cantidad de elementos en "direccion".
 # 7.Unir dos tuplas creando una tercera con el contenido de ambas.
+
+direccion = ('Calle Rosario', 10, 'Madrid', 20919, 'España')
+
+print(direccion.index('Madrid'))
+print(direccion[-2:])
+
+nueva_direccion = list(direccion)
+nueva_direccion[1] = 15
+nueva_direccion = tuple(nueva_direccion)
+
+print(direccion.count('Madrid'))
+print(len(direccion))
+
+union_direcciones = direccion + nueva_direccion
+
+print(union_direcciones)
 
 ################### EJERCICIOS DE DICCIONARIOS #####################
 
@@ -55,13 +126,47 @@
 # 5.Usa el metodo "items()" para ver todos los contactos y sus numero de telefono
 # 6.Usa el metodo "get()" para buscar un contacto específico "Lucia"
 
+agenda = {"Juan": "555-555-5555", "Maria": "444-444-4444", "Pedro":"333-333-3333"}
+agenda.update({"Lucia":"222-222-2222"})
+agenda.pop("Juan")
+for nombre, telefono in agenda.items():
+    print(f"{nombre} tiene el telefono {telefono}.")
+telefono_lucia = agenda.get("Lucia")
 
 ################### EJERCICIO 2 #####################
 
 # 1.Crea un diccionario vacío llamado "estudiantes" que almacene información de los estudiantes de una clase.
-# 2.Agrega algunos elementos al diccionario, como por ejemplo, "Juan": {"edad":22, "notas": [8, 9, 10, 8], "asistencia": {"Lunes": True, "Martes": True, "Miercoles": False, "Jueves": True, "Viernes": True}}, "Maria": {"edad":20, "notas": [6, 7, 8, 9], "asistencia": {"Lunes": True, "Martes": False, "Miercoles": True, "Jueves": False, "Viernes": True}}
-# 3.Utiliza el método "update()"" para agregar un nuevo estudiante "Lucia":{"edad":21, "notas": [9, 8, 7, 6], "asistencia": {"Lunes": False, "Martes": True, "Miercoles": True, "Jueves": True, "Viernes": False}}
-# 4.Utiliza el método "pop()" para eliminar el estudiante "Juan"
-# 5.Utiliza un ciclo for para calcular el promedio de notas de cada estudiante y añadirlo al diccionario
-# 6.Utiliza el método "get()"" para buscar la asistencia de un estudiante específico "Lucia"
-# 7.Utiliza un ciclo for para contar el número de veces que un estudiante ha faltado a clase y añadirlo al diccionario.
+# 2.Agrega algunos elementos al diccionario, como por ejemplo, 
+# "Juan": {"edad":22, "notas": [8, 9, 10, 8], "asistencia": {"Lunes": True, "Martes": True, "Miercoles": False, "Jueves": True, "Viernes": True}}, 
+# "Maria": {"edad":20, "notas": [6, 7, 8, 9], "asistencia": {"Lunes": True, "Martes": False, "Miercoles": True, "Jueves": False, "Viernes": True}}
+# 3.Utiliza el método "update()"" para agregar un nuevo estudiante 
+# "Lucia":{"edad":21, "notas": [9, 8, 7, 6], "asistencia": {"Lunes": False, "Martes": True, "Miercoles": True, "Jueves": True, "Viernes": False}}
+# 4.Utiliza un ciclo for para calcular el promedio de notas de cada estudiante y añadelo al diccionario de informacion del estudiante.
+# 5.Utiliza el método "get()"" para buscar la asistencia de un estudiante específico "Lucia"
+# 6.Utiliza un ciclo for para contar el número de veces que un estudiante ha faltado a clase y añadirlo al diccionario.
+# 7.Utiliza el método "pop()" para eliminar el estudiante "Juan"
+
+estudiantes = {"Juan": {"edad":22, "notas": [8, 9, 10, 8], "asistencia": {"Lunes": True, "Martes": True, "Miercoles": False, "Jueves": True, "Viernes": True}}, 
+               "Maria": {"edad":20, "notas": [6, 7, 8, 9], "asistencia": {"Lunes": True, "Martes": False, "Miercoles": True, "Jueves": False, "Viernes": True}}}
+
+estudiantes.update({"Lucia":{"edad":21, "notas": [9, 8, 7, 6], "asistencia": {"Lunes": False, "Martes": True, "Miercoles": True, "Jueves": True, "Viernes": False}}})
+
+for nombre, informacion in estudiantes.items():
+    notas = informacion['notas']
+    suma_notas = 0
+    for nota in notas:
+        suma_notas = suma_notas + nota
+    promedio = suma_notas / len(notas)
+    informacion.update({"Promedio": promedio})
+
+for nombre, informacion in estudiantes.items():
+    asistencia = informacion['asistencia']
+    numero_falta_asistencia = 0
+    for dia, valor_asistencia in asistencia.items():
+        if valor_asistencia == False:
+            numero_falta_asistencia = numero_falta_asistencia + 1
+    informacion.update({'Dias_fallados':numero_falta_asistencia})
+    
+print(estudiantes)
+estudiantes.pop("Juan")
+        
--- 4.Funciones/ejercicios/soluciones.py
+++ 4.Funciones/ejercicios/soluciones.py
@@ -2,7 +2,8 @@
 
 ################### EJERCICIO 1 #####################
 
-# 1.Crea una función llamada "calcular_impuestos" que reciba dos argumentos: "ingresos" y "tasa_impuestos" (por ejemplo: calcular_impuestos(50000, 0.25))
+# 1.Crea una función llamada "calcular_impuestos" que reciba dos argumentos: "ingresos" (int) y "tasa_impuestos" (float) y retorne los impuestos a pagar (float)
+#   (por ejemplo: calcular_impuestos(50000, 0.25))
 # 2.Dentro de la función, calcula el impuesto debido multiplicando los ingresos por la tasa de impuestos y guarda el resultado en una variable "impuesto_debido".
 # 3.Retorna el valor de "impuesto_debido".
 # 4.Crea una variable "ingresos" con el valor de tus ingresos anuales (por ejemplo: 75000)
@@ -10,19 +11,42 @@
 # 6.Crea una variable "impuesto_a_pagar" que sea igual a la función "calcular_impuestos" con los argumentos "ingresos" y "tasa_impuestos".
 # 7.Imprime "Impuesto a pagar: " y el valor de "impuesto_a_pagar".
 
+def calcular_impuestos(ingresos : int, tasa_impuestos: float) -> float:
+    impuesto_debido = ingresos * tasa_impuestos
+    return impuesto_debido
+
+ingresos = 75000
+tasa_impuestos = 0.3
+impuestos_a_pagar = calcular_impuestos(ingresos,tasa_impuestos)
+print(f"Impuesto a pagar: {impuestos_a_pagar}")
+    
+
 ################### EJERCICIO 2 #####################
 
-# 1.Crea una función llamada "calcular_promedio" que reciba una lista de números como argumento.
+# 1.Crea una función llamada "calcular_promedio" que reciba una lista de números como argumento llamada "numeros" y retorne el promedio (float).
 # 2.Dentro de la función, calcula el promedio de los números en la lista y guarda el resultado en una variable "promedio".
 # 3.Retorna el valor de "promedio".
 # 4.Crea una lista de números con las calificaciones obtenidas en un curso (por ejemplo: [9, 8, 10, 7, 8, 9, 6, 8, 10, 9])
 # 5.Llama a la función "calcular_promedio" con la lista de calificaciones como argumento y guarda el resultado en una variable "promedio_curso"
 # 6.Imprime "El promedio del curso es: " y el valor de "promedio_curso"
 
+def calcular_promedio(numeros:list) -> float:
+    suma_numeros = 0
+    for numero in numeros:
+        suma_numeros = suma_numeros + numero
+    promedio = suma_numeros / len(numeros)
+    return promedio
+
+calificaciones = [9, 8, 10, 7, 8, 9, 6, 8, 10, 9]
+promedio_curso = calcular_promedio(calificaciones)
+print(f"El promedio del curso es: {promedio_curso}")
+
 ################### EJERCICIO 3 #####################
 
-# 1.Crea una función llamada "calcular_costo_envio" que reciba dos argumentos: "origen" y "destino" (por ejemplo: calcular_costo_envio("Nueva York", "Los Ángeles"))
-# 2.Dentro de la función, crea un diccionario con los costos de envio entre diferentes ciudades (por ejemplo: {"Nueva York": {"Los Ángeles": 20, "Chicago": 10, "Miami": 15}, "Los Ángeles": {"Nueva York": 25, "Chicago": 17, "Miami": 20}, ...})
+# 1.Crea una función llamada "calcular_costo_envio" que reciba dos argumentos: "origen" (str) y "destino" (str) y retorne el precio (int) 
+#   (por ejemplo: calcular_costo_envio("Nueva York", "Los Ángeles"))
+# 2.Dentro de la función, crea un diccionario con los costos de envio entre diferentes ciudades 
+#   (por ejemplo: {"Nueva York": {"Los Ángeles": 20, "Chicago": 10, "Miami": 15}, "Los Ángeles": {"Nueva York": 25, "Chicago": 17, "Miami": 20}})
 # 3.Usa los argumentos "origen" y "destino" para obtener el costo de envio entre esas dos ciudades del diccionario y guarda el resultado en una variable "costo"
 # 4.Retorna el valor de "costo"
 # 5.Crea una variable "origen" con el lugar de origen del envio (por ejemplo: "Nueva York")
@@ -30,9 +54,22 @@
 # 7.Llama a la función "calcular_costo_envio" con las variables "origen" y "destino" como argumentos y guarda el resultado en una variable "costo_envio"
 # 8.Imprime "El costo de envio desde " + origen + " hasta " + destino + " es de: " + costo_envio
 
+def calcular_costo_envio(origen: str, destino:str) -> int:
+    informacion_costos = {"Nueva York": {"Los Ángeles": 20, "Chicago": 10, "Miami": 15}, "Los Ángeles": {"Nueva York": 25, "Chicago": 17, "Miami": 20}}
+    informacion_destinos = informacion_costos[origen]
+    coste_destino = informacion_destinos.get(destino,f"Desde {origen} hasta {destino} no hay ruta posible de envio.")
+    return coste_destino
+        
+origen= 'Nueva York'
+destino = 'Los Ángeles'
+
+costo_envio = calcular_costo_envio(origen, destino)
+print(f"El costo de envio desde {origen} hasta {destino} es de: {costo_envio}")
+
 ################### EJERCICIO 4 #####################
 
-# 1.Crea una función llamada "verificar_stock" que reciba un argumento: "productos" (por ejemplo: verificar_stock(productos))
+# 1.Crea una función llamada "verificar_stock" que reciba una lista de diccionarios como argumento llamada "productos" y retorne una lista de productos agotados. 
+#   (Ejemplo: [{"nombre": "Laptop", "precio": 800, "stock": 10},    {"nombre": "Teclado", "precio": 30, "stock": 20}])
 # 2.Dentro de la función, crea una lista vacia llamada "productos_agotados" para almacenar los productos que ya no tienen stock
 # 3.Usa un bucle "for" para recorrer la lista "productos" y verificar el stock de cada producto
 # 4.Si el stock es 0, agrega el producto a la lista "productos_agotados"
@@ -43,26 +80,73 @@
 # {"nombre": "Cargador", "precio": 20, "stock": 0},    {"nombre": "Audifonos", "precio": 25, "stock": 30},    
 # {"nombre": "Memoria USB", "precio": 10, "stock": 100},    {"nombre": "Disco duro externo", "precio": 75, "stock": 20}])
 # 7.Llama a la función "verificar_stock" con la variable "productos" como argumentos y guarda el resultado en una variable "productos_agotados"
-# 8.Imprime "Los productos agotados son: " y el valor de "productos_agotados"
+# 8.Imprime con un bucle "for" sobre "productos_agotados" para cada producto el siguiente mensaje "El producto: {producto} con el precio {precio} ha sido agotado".
 
+def verificar_stock(productos: list) -> list:
+    productos_agotados = []
+    for producto in productos:
+        stock = producto['stock']
+        if stock == 0:
+            productos_agotados.append(producto)
+    return productos_agotados
+    
+lista_productos = [{"nombre": "Laptop", "precio": 800, "stock": 10},    {"nombre": "Teclado", "precio": 30, "stock": 20},    {"nombre": "Mouse", "precio": 15, "stock": 50},    
+ {"nombre": "Monitor", "precio": 200, "stock": 5},    {"nombre": "Impresora", "precio": 80, "stock": 0},    {"nombre": "Parlantes", "precio": 40, "stock": 15},    
+ {"nombre": "Cargador", "precio": 20, "stock": 0},    {"nombre": "Audifonos", "precio": 25, "stock": 30},    
+ {"nombre": "Memoria USB", "precio": 10, "stock": 100},    {"nombre": "Disco duro externo", "precio": 75, "stock": 20}]
+
+productos_agotados = verificar_stock(lista_productos)
+for producto_agotado in productos_agotados:
+    print(f"El producto {producto_agotado['nombre']} con el precio {producto_agotado['precio']} ha sido agotado.")
+    
 ################### EJERCICIO 5 #####################
 
 # 1.Crea una función llamada "analizar_datos" que reciba un argumento "pacientes", que es una lista de diccionarios con información de pacientes (nombre, edad, sexo, enfermedad).
 # 2.Dentro de la función, crea variables vacías para almacenar información estadística, como "pacientes_masculinos", "pacientes_femeninos", "pacientes_mayores", "pacientes_menores", "pacientes_enfermos" y "pacientes_sanos".
-# 3.Usando un bucle "for" recorre la lista "pacientes" y analiza cada paciente:
-#   3.1.Si el paciente es masculino, aumenta en 1 la variable "pacientes_masculinos"
-#   3.2.Si el paciente es femenino, aumenta en 1 la variable "pacientes_femeninos"
-#   3.3.Si el paciente tiene más de 18 años, aumenta en 1 la variable "pacientes_mayores"
-#   3.4.Si el paciente tiene menos de 18 años, aumenta en 1 la variable "pacientes_menores"
-#   3.5.Si el paciente tiene una enfermedad, aumenta en 1 la variable "pacientes_enfermos"
-#   3.6.Si el paciente no tiene una enfermedad, aumenta en 1 la variable "pacientes_sanos"
+# 3.Usando un bucle "for" recorre la lista "pacientes" y analiza cada paciente añadiendo cada estadistica a un diccionario llamado "estadisticas":
+#   3.1.Si el paciente es masculino, aumenta en 1 el valor de la clave "pacientes_masculinos" del diccionario
+#   3.2.Si el paciente es femenino, aumenta en 1 el valor de la clave "pacientes_femeninos" del diccionario
+#   3.3.Si el paciente tiene más de 18 años, aumenta en 1 el valor de la clave "pacientes_mayores" del diccionario
+#   3.4.Si el paciente tiene menos de 18 años, aumenta en 1 el valor de la clave "pacientes_menores" del diccionario
+#   3.5.Si el paciente tiene una enfermedad, aumenta en 1 el valor de la clave "pacientes_enfermos" del diccionario
+#   3.6.Si el paciente no tiene una enfermedad, aumenta en 1 el valor de la clave "pacientes_sanos" del diccionario
 # 4.Crea una variable "pacientes" con la lista de diccionarios con información de los pacientes (por ejemplo: 
-# [{{ "nombre": "Pedro", "edad": 45, "sexo": "masculino", "enfermedad": "hipertensión" }
+# [{ "nombre": "Pedro", "edad": 45, "sexo": "masculino", "enfermedad": "hipertensión" }
 #   { "nombre": "Ana", "edad": 35, "sexo": "femenino", "enfermedad": "artritis" }
 #   { "nombre": "Juan", "edad": 25, "sexo": "masculino", "enfermedad": None }
 #   { "nombre": "Sofia", "edad": 40, "sexo": "femenino", "enfermedad": "migraña" }
 #   { "nombre": "Carlos", "edad": 55, "sexo": "masculino", "enfermedad": "colesterol alto" }
-#   { "nombre": "Isabel", "edad": 30, "sexo": "femenino", "enfermedad": "asma" }]).
-# 5.Llama a la función "analizar_datos" con la variable "pacientes" como argumento.
-# 6.Dentro de la función "analizar_datos", imprime las estadísticas obtenidas, como "Pacientes masculinos: ", "Pacientes femeninos: ", "Pacientes mayores de 18 años: ", 
-# "Pacientes menores de 18 años: ", "Pacientes enfermos: " y "Pacientes sanos: ".
+#   { "nombre": "Isabel", "edad": 30, "sexo": "femenino", "enfermedad": "asma" }].
+# 5.Llama a la función "analizar_datos" con la variable "pacientes" como argumento y guarda el resultado en una variable llamada "Estadisticas".
+# 6.Imprime el diccionario con la función "print()" y revisa que los valores sean los correctos.
+
+def analizar_datos(pacientes:list) -> dict:
+    estadisticas = {}
+    for paciente in pacientes:
+        if paciente['sexo'] == 'masculino':
+            estadisticas['pacientes_masculinos'] = estadisticas.get('pacientes_masculinos', 0) + 1
+        elif paciente['sexo'] == 'femenino':
+            estadisticas['pacientes_femeninos'] = estadisticas.get('pacientes_femeninos', 0) + 1
+            
+        if paciente['edad'] >= 18:
+            estadisticas['pacientes_mayores'] = estadisticas.get('pacientes_mayores', 0) + 1
+        elif paciente['edad'] < 18:
+            estadisticas['pacientes_menores'] = estadisticas.get('pacientes_menores', 0) + 1
+            
+        if paciente['enfermedad'] is not None:
+            estadisticas['pacientes_enfermos'] = estadisticas.get('pacientes_enfermos', 0) + 1
+        elif paciente['enfermedad'] is None:
+            estadisticas['pacientes_sanos'] = estadisticas.get('pacientes_sanos', 0) + 1
+            
+    return estadisticas
+
+pacientes = [{ "nombre": "Pedro", "edad": 45, "sexo": "masculino", "enfermedad": "hipertensión" },
+   { "nombre": "Ana", "edad": 35, "sexo": "femenino", "enfermedad": "artritis" },
+   { "nombre": "Juan", "edad": 25, "sexo": "masculino", "enfermedad": None },
+   { "nombre": "Sofia", "edad": 40, "sexo": "femenino", "enfermedad": "migraña" },
+   { "nombre": "Carlos", "edad": 55, "sexo": "masculino", "enfermedad": "colesterol alto" },
+   { "nombre": "Isabel", "edad": 30, "sexo": "femenino", "enfermedad": "asma" }]
+
+estadisticas = analizar_datos(pacientes)
+print(estadisticas)
+    
